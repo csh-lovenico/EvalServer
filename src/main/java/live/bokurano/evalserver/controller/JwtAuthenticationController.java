f6928c6ad3ef38dbb2c812809fd8caa3af367230
Shuhuai Cao <csh980717@outlook.com>
2020-03-22 12:34:18 +0800

    格式化代码

--- src/main/java/live/bokurano/evalserver/controller/JwtAuthenticationController.java
+++ src/main/java/live/bokurano/evalserver/controller/JwtAuthenticationController.java
@@ -46,9 +46,9 @@ public class JwtAuthenticationController {
 		try {
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
+			throw new DisabledException("USER_DISABLED", e);
 		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
+			throw new BadCredentialsException("INVALID_CREDENTIALS", e);
 		}
 	}
 
