748f7e0a36239948ff0bdd7f3041cd34f2f33247
Shuhuai Cao <csh980717@outlook.com>
2020-04-16 20:17:00 +0800

    测试连接用接口

--- src/main/java/live/bokurano/evalserver/controller/GeneralController.java
+++ src/main/java/live/bokurano/evalserver/controller/GeneralController.java
@@ -3,23 +3,18 @@ package live.bokurano.evalserver.controller;
 import live.bokurano.evalserver.common.JsonResult;
 import live.bokurano.evalserver.common.ServerResult;
 import live.bokurano.evalserver.service.EvaluationService;
-import live.bokurano.evalserver.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @RestController
 public class GeneralController {
 	@Autowired
 	private EvaluationService evaluationService;
-
-	@Autowired
-	private JwtUserDetailsService userDetailsService;
 
 	@GetMapping("/api/getCourseEvaluation")
 	public JsonResult getCourseEvaluation(@RequestParam("courseId") String courseId) {
@@ -47,9 +42,8 @@ public class GeneralController {
 		return result;
 	}
 
-	@GetMapping("/api/getCurrentRole")
-	public String getCurrentRole(@RequestParam("username") String username) {
-		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-		return userDetails.getAuthorities().stream().collect(Collectors.toList()).get(0).getAuthority();
+	@GetMapping("/api/test")
+	public Map<String, String> connectionTest() {
+		return Map.of("status", "ok");
 	}
 }
